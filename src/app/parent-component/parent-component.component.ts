9cc6f7f2a1f28d229529b1e431f88eedb788d618
Muhammad Huzaifa Khan <huzaifa18072002@gmail.com>
2023-06-23 10:19:12 +0500

    Project Added

---
+++ src/app/parent-component/parent-component.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+@Component({
+  selector: 'app-parent-component',
+  templateUrl: './parent-component.component.html',
+  styleUrls: ['./parent-component.component.css']
+})
+
+
+export class ParentComponentComponent implements OnInit {
+  get_person(){
+    const person = [{name:'John' , profession:'Architect' , img:"../../assets/avatar1.png"},
+    {name:'Bob' , profession:'Civil Engineer' , img:"../../assets/avatar2.png"},
+    {name:'Alex' , profession:'Computer Scientist' , img:"../../assets/avatar3.png"},
+    {name:'Andrew' , profession:'Mechanical Engineer' , img:"../../assets/avatar4.png"},]
+    return person
+  }
+  parent_list : any = []
+  
+  ngOnInit(){
+    this.parent_list = this.get_person()
+  }
+
+}
